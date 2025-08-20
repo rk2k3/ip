ca7ab7ede9fb8ec4db1a670a2a2938251ae73922
ron <ronkoh2511@gmail.com>
2025-08-20 22:11:59 +0800

    Add delete task functionality

--- src/main/java/Mip.java
+++ src/main/java/Mip.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Mip {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Mip\nWhat can I do for you?\n");
 
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         String command;
         Scanner input = new Scanner(System.in);
 
@@ -15,25 +16,25 @@ public class Mip {
                     System.out.println("Goodbye! Mip WILL see you again.");
                     break;
                 } else if (command.equals("list")) { // List out all previous commands
-                    if (Task.getCount() == 0) {
+                    if (tasks.isEmpty()) {
                         throw new MipException("Your task list is empty.");
                     }
-                    for (int i = 1; i <= Task.getCount(); i++) {
-                        System.out.println(i + "." + tasks[i]);
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + "." + tasks.get(i));
                     }
                     System.out.println(); // Add a new line at the end
                 } else if (command.matches("mark \\d+")) {
                     String numberPart = command.replaceAll("\\D+", "");
                     int number = Integer.parseInt(numberPart);
-                    tasks[number].setCompleted(true);
+                    tasks.get(number - 1).setCompleted(true);
                     System.out.println("Super amazing!!! I've marked this task as done:");
-                    System.out.println(tasks[number] + "\n");
+                    System.out.println(tasks.get(number - 1) + "\n");
                 } else if (command.matches("unmark \\d+")) {
                     String numberPart = command.replaceAll("\\D+", "");
                     int number = Integer.parseInt(numberPart);
-                    tasks[number].setCompleted(false);
+                    tasks.get(number - 1).setCompleted(false);
                     System.out.println("I've marked this task as not done:");
-                    System.out.println(tasks[number] + "\n");
+                    System.out.println(tasks.get(number - 1) + "\n");
                 } else if (command.isEmpty()) {
                     continue;
                 } else if (command.startsWith("todo")) {
@@ -44,11 +45,11 @@ public class Mip {
                     }
 
                     TodoTask todoTask = new TodoTask(task);
-                    tasks[Task.getCount()] = todoTask;
+                    tasks.add(todoTask);
 
                     System.out.println("Understood, I've added this task:");
                     System.out.println(todoTask);
-                    System.out.println("The number of your tasks is: " + Task.getCount() + "\n");
+                    System.out.println("The number of your tasks is: " + tasks.size() + "\n");
                 } else if (command.startsWith("deadline")) {
                     String rest = command.substring(9); // skip "deadline "
 
@@ -63,14 +64,14 @@ public class Mip {
                         throw new MipException("A deadline must have '/by' followed by a time.");
                     }
                     String task = parts[0];
-                    String deadline = parts.length > 1 ? parts[1] : "";
+                    String deadline = parts[1];
 
                     DeadlineTask deadlineTask = new DeadlineTask(task, deadline);
-                    tasks[Task.getCount()] = deadlineTask;
+                    tasks.add(deadlineTask);
 
                     System.out.println("Understood, I've added this task:");
                     System.out.println(deadlineTask);
-                    System.out.println("The number of your tasks is: " + Task.getCount() + "\n");
+                    System.out.println("The number of your tasks is: " + tasks.size() + "\n");
                 } else if (command.startsWith("event")) {
                     String rest = command.substring(6); // skip "event "
                     if (rest.isEmpty()) {
@@ -97,10 +98,15 @@ public class Mip {
                     String to = parts2[1];
                     EventTask eventTask = new EventTask(task, from, to);
 
-                    tasks[Task.getCount()] = eventTask;
+                    tasks.add(eventTask);
                     System.out.println("Understood, I've added this task:");
                     System.out.println(eventTask);
-                    System.out.println("The number of your tasks is: " + Task.getCount() + "\n");
+                    System.out.println("The number of your tasks is: " + tasks.size() + "\n");
+                } else if (command.matches("delete \\d+")) {
+                    String numberPart = command.replaceAll("\\D+", "");
+                    int number = Integer.parseInt(numberPart);
+                    System.out.println("I have deleted this task:");
+                    System.out.println(tasks.remove(number - 1) + "\n");
                 } else {
                     throw new MipException("I have no idea what you are talking about...");
                 }
