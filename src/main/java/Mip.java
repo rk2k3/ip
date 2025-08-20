1f1ade99239acab0276f3ddbf1b49a53e6c83481
ron <ronkoh2511@gmail.com>
2025-08-20 17:05:09 +0800

    Add ability to mark task as done/not done
    Add the ability to mark tasks as done.
    add the ability to change the status back to not done.

--- src/main/java/Mip.java
+++ src/main/java/Mip.java
@@ -4,23 +4,36 @@ public class Mip {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Mip\nWhat can I do for you?\n");
 
-        String[] commandList = new String[100];
-        int commandIndex = 0;
+        Task[] tasks = new Task[100];
         String command;
         Scanner input = new Scanner(System.in);
+
         while (true){
             command = input.nextLine();
             if (command.equals("bye")) {
                 System.out.println("Goodbye! Mip WILL see you again.");
                 break;
             } else if (command.equals("list")) { // List out all  previous commands
-                for (int i = 0; i < commandIndex; i++) {
-                    System.out.println((i + 1) + ". " + commandList[i]);
+                for (int i = 0; i < Task.getCount(); i++) {
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 System.out.println(); // Add a new line at the end
+            } else if (command.matches("mark \\d+")) {
+                String numberPart = command.replaceAll("\\D+", "");
+                int number = Integer.parseInt(numberPart);
+                tasks[number - 1].setCompleted(true);
+                System.out.println("Super amazing!!! I've marked this task as done:");
+                System.out.println(tasks[number - 1] + "\n");
+            } else if (command.matches("unmark \\d+")) {
+                String numberPart = command.replaceAll("\\D+", "");
+                int number = Integer.parseInt(numberPart);
+                tasks[number - 1].setCompleted(false);
+                System.out.println("I've marked this task as not done:");
+                System.out.println(tasks[number - 1] + "\n");
+            } else if (command.isEmpty()) {
+                continue;
             } else {
-                commandList[commandIndex] = command;
-                commandIndex++;
+                tasks[Task.getCount()] = new Task(command);
                 System.out.println("Added: " + command + "\n");
             }
         }
