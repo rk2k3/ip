e8acb79b221668b2c6fa48fde4564659687f5f43
ron <ronkoh2511@gmail.com>
2025-08-20 16:20:28 +0800

    Add List tasks functionality to Mip
    Add the ability to store whatever text entered by the user
    and display them back to the user when requested.

--- src/main/java/Mip.java
+++ src/main/java/Mip.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Mip {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Mip\nWhat can I do for you?\n\n");
+        System.out.println("Hello! I'm Mip\nWhat can I do for you?\n");
 
+        String[] commandList = new String[100];
+        int commandIndex = 0;
         String command;
         Scanner input = new Scanner(System.in);
         while (true){
@@ -11,8 +13,15 @@ public class Mip {
             if (command.equals("bye")) {
                 System.out.println("Goodbye! Mip WILL see you again.");
                 break;
+            } else if (command.equals("list")) { // List out all  previous commands
+                for (int i = 0; i < commandIndex; i++) {
+                    System.out.println((i + 1) + ". " + commandList[i]);
+                }
+                System.out.println(); // Add a new line at the end
             } else {
-                System.out.println(command + "\n");
+                commandList[commandIndex] = command;
+                commandIndex++;
+                System.out.println("Added: " + command + "\n");
             }
         }
     }
