ba35ef2c1fe62cf3c6cfa1541dd5d4f0ca37b60d
ron <ronkoh2511@gmail.com>
2025-08-20 16:00:23 +0800

    Add Echo functionality to Mip
    Mip will now echo the commands of the user, until the user says bye.

--- src/main/java/Mip.java
+++ src/main/java/Mip.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Mip {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Mip\nWhat can I do for you?\n\nBye. Hpe to see you again soon!");
+        System.out.println("Hello! I'm Mip\nWhat can I do for you?\n\n");
+
+        String command;
+        Scanner input = new Scanner(System.in);
+        while (true){
+            command = input.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Goodbye! Mip WILL see you again.");
+                break;
+            } else {
+                System.out.println(command + "\n");
+            }
+        }
     }
 }
